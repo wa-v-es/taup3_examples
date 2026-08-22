6ea4b1b64be9d9e797b41cef86032b7991bd8d6a
shubham-688 <agr.shubh.agr@gmail.com>
2026-08-22 14:37:37 -0400

    changed EllipciPy distances to spherical!

--- errors_misuse/contour_geod_geoc.py
+++ errors_misuse/contour_geod_geoc.py
@@ -18,7 +18,7 @@ from matplotlib.colors import TwoSlopeNorm,CenteredNorm
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from ellipticipy import ellipticity_correction
 from obspy.taup import TauPyModel
-from obspy.geodetics.base import gps2dist_azimuth, kilometers2degrees
+from obspy.geodetics.base import gps2dist_azimuth, kilometers2degrees, locations2degrees
 
 ###
 
@@ -96,6 +96,21 @@ def plot_dist_diff(station_data,param1='dist_geod',param2='dist_sph',eq_lon=0,eq
     plt.savefig(figname,dpi=600,bbox_inches='tight', pad_inches=0.1)
     plt.show()
 
+def spherical_dist_azi(lat_s, lon_s, lat_r, lon_r):
+    sph_dist_deg = locations2degrees(lat_s, lon_s, lat_r, lon_r) #obspy haversine
+
+    phi_s = np.radians(lat_s)
+    phi_r = np.radians(lat_r)
+    dlon = np.radians(lon_r - lon_s)
+
+    azimuth = np.degrees(np.arctan2(np.sin(dlon) * np.cos(phi_r),
+        np.cos(phi_s) * np.sin(phi_r)
+        - np.sin(phi_s) * np.cos(phi_r) * np.cos(dlon)))
+
+    azimuth %= 360
+
+    return sph_dist_deg, azimuth
+
 def ellipticipy_py_corr(phase=['SS'],spacing=5, max_dist=50,eq_lon=0,eq_lat=45):
     """
     Uses ellipticipy (https://github.com/StuartJRussell/EllipticiPy/tree/master/src)
@@ -106,23 +121,23 @@ def ellipticipy_py_corr(phase=['SS'],spacing=5, max_dist=50,eq_lon=0,eq_lat=45):
     eventdepth=30
     station_data_epy=[]
     for st in stations:
-
-         dist_m, az, baz = gps2dist_azimuth(eq_lat, eq_lon, st[0], st[1])
-         dist_d=kilometers2degrees(dist_m / 1000.0)
-         arrivals = model.get_ray_paths(source_depth_in_km = eventdepth, distance_in_degree = dist_d, phase_list = phase)
-         correction=ellipticity_correction(arrivals, azimuth = az, source_latitude = eq_lat)
+         dist_sp_deg,azi_sph=spherical_dist_azi(eq_lat, eq_lon, st[0], st[1])
+         # dist_m, az, baz = gps2dist_azimuth(eq_lat, eq_lon, st[0], st[1])
+         # dist_d=kilometers2degrees(dist_m / 1000.)
+         arrivals = model.get_ray_paths(source_depth_in_km = eventdepth, distance_in_degree = dist_sp_deg, phase_list = phase)
+         correction=ellipticity_correction(arrivals, azimuth = azi_sph, source_latitude = eq_lat)
          # print(f" correction val:  {correction}, for dist={dist_d}")
 
          station_data_epy.append({
              'lat': st[0],
              'lon': st[1],
-             'dist_geod_obs': dist_d,
-             'time_geod': arrivals[0].time,
+             'dist_sp_deg': dist_sp_deg,
+             'time_sph': arrivals[0].time,
              'time_correction': correction[0],
              'phase': phase})
 
     print(f"lenth of station data ePy: {len(station_data_epy)}")
-    return station_data_epy
+    return station_data_epy,arrivals
 ###
 def cal_dist_time(taupserver,phase=['P'],spacing=5, max_dist=50,eq_lon=0,eq_lat=45):
     phases=(phase)
@@ -181,7 +196,7 @@ def main():
     # with taup.TauPServer(taup_path=taup_path) as taupserver:
     #     station_data=cal_dist_time(taupserver,phase=['SS'],spacing=20, max_dist=170,eq_lon=0,eq_lat=45)
 
-    station_data_epy=ellipticipy_py_corr(phase=['SS'],spacing=5, max_dist=170,eq_lon=0,eq_lat=45)
+    station_data_epy,arr=ellipticipy_py_corr(phase=['SS'],spacing=5, max_dist=170,eq_lon=0,eq_lat=45)
 
     # distance differences
     # plot_dist_diff(station_data,'dist_geoc','dist_geod',eq_lon=0,eq_lat=45,cmap='PRGn',label="$ \Delta $ Distance ($^\\circ$)",tick_space=.1,figname='geod_geoc_dist_d.png')
@@ -190,8 +205,15 @@ def main():
     # plot_dist_diff(station_data,'time_geoc','time_geod',eq_lon=0,eq_lat=45,cmap='RdGy',label="$ \Delta $ Time (s)",tick_space=.5,figname='geod_geoc_time_d.png')
     # plot_dist_diff(station_data,'time_geoc','time_sph',eq_lon=0,eq_lat=45,cmap='RdGy',label="$ \Delta $ Time (s)",tick_space=1,figname='geoc_time_d.png')
     ###### time correction using ellipticipy
-    plot_dist_diff(station_data_epy,'time_correction','None',eq_lon=0,eq_lat=45,cmap='RdGy',label="Ellipticity Correction (s)",tick_space=1,figname='ellip_py_geod_corr.png')
+    plot_dist_diff(station_data_epy,'time_correction','None',eq_lon=0,eq_lat=45,cmap='RdGy',label="Ellipticity Correction (s)",tick_space=1,figname='ellip_py_sphr_dist_corr.png')
 
+    # diff=[]
+    # corr=[]
+    # for i,data in enumerate(station_data):
+    #     geod_taup=station_data[i]['dist_sph']
+    #     geod_obspy=station_data_epy[i]['dist_sp_deg']
+    #     diff.append(geod_taup-geod_obspy)
+    #     corr.append(station_data_epy[i]['time_correction'])
 
 if __name__ == '__main__':
     main()
@@ -199,10 +221,3 @@ if __name__ == '__main__':
 sys.exit()
 
 ####
-# diff=[]
-# corr=[]
-# for i,data in enumerate(station_data):
-#     geod_taup=station_data[i]['dist_geod']
-#     geod_obspy=station_data_epy[i]['dist_geod_obs']
-#     diff.append(geod_taup-geod_obspy)
-#     corr.append(station_data_epy[i][])
